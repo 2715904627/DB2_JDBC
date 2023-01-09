5a9fc3cbfdfc1199c89dc1b4c7072f23e88ee547
Kadupul <2715904627@qq.com>
2023-01-09 09:54:57 +0800

    modified table to templ(employee)

--- src/com/cxf55200132/ui/Add.java
+++ src/com/cxf55200132/ui/Add.java
@@ -1,7 +1,9 @@
 package com.cxf55200132.ui;
 
 import com.cxf55200132.DAO.Dao.ACTDAO;
+import com.cxf55200132.DAO.Dao.TEMPLDAO;
 import com.cxf55200132.DAO.Domain.ACT;
+import com.cxf55200132.DAO.Domain.TEMPL;
 
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
@@ -11,8 +13,9 @@ import java.util.List;
 public class Add {
 
 
-    private ACTDAO actdao = new ACTDAO();
-    String[] columnNames = {"ACTNO","ACTKWD","ACTDESC"};
+    private TEMPLDAO templdao = new TEMPLDAO();
+    String[] columnNames = {"EMPNO","FIRSTNME","MIDINIT","LASTNAME","WORKDEPT","PHONENO","HIREDATE","JOB","EDLEVEL","SEX","BIRTHDATE","SALARY","BONUS","COMM"};
+
 
     private JTable tabData;
     private JButton btn_single;
@@ -44,13 +47,24 @@ public class Add {
 
 
     private  void InitTable() throws SQLException {
-        List<ACT> list = actdao.QueryMultiply("SELECT * FROM act WHERE ACTNO >= ?", ACT.class,20);
+        List<TEMPL> list = templdao.QueryMultiply("SELECT * FROM employee", TEMPL.class);
 
-        Object[][] data = new Object[list.size()][3];
+        Object[][] data = new Object[list.size()][14];
         for (int i = 0; i < data.length ; i++) {
-            data[i][0] = list.get(i).getACTNO();
-            data[i][1] = list.get(i).getACTKWD();
-            data[i][2] = list.get(i).getACTDESC();
+            data[i][0] = list.get(i).getEMPNO();
+            data[i][1] = list.get(i).getFIRSTNME();
+            data[i][2] = list.get(i).getMIDINIT();
+            data[i][3] = list.get(i).getLASTNAME();
+            data[i][4] = list.get(i).getWORKDEPT();
+            data[i][5] = list.get(i).getPHONENO();
+            data[i][6] = list.get(i).getHIREDATE();
+            data[i][7] = list.get(i).getJOB();
+            data[i][8] = list.get(i).getEDLEVEL();
+            data[i][9] = list.get(i).getSEX();
+            data[i][10] = list.get(i).getBIRTHDATE();
+            data[i][11] = list.get(i).getSALARY();
+            data[i][12] = list.get(i).getBONUS();
+            data[i][13] = list.get(i).getCOMM();
 
         }
         DefaultTableModel dtm = new DefaultTableModel(data, columnNames);
--- src/com/cxf55200132/ui/MainTab.java
+++ src/com/cxf55200132/ui/MainTab.java
@@ -1,9 +1,12 @@
 package com.cxf55200132.ui;
 
 import com.cxf55200132.DAO.Dao.ACTDAO;
+import com.cxf55200132.DAO.Dao.TEMPLDAO;
 import com.cxf55200132.DAO.Domain.ACT;
+import com.cxf55200132.DAO.Domain.TEMPL;
 import com.cxf55200132.view.MainForm;
 
+import javax.management.StandardEmitterMBean;
 import javax.swing.*;
 import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
@@ -11,11 +14,12 @@ import javax.swing.table.DefaultTableModel;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.sql.SQLException;
+import java.util.Date;
 import java.util.List;
 
 public class MainTab {
-    private ACTDAO actdao = new ACTDAO();
-    String[] columnNames = {"ACTNO","ACTKWD","ACTDESC"};
+    private TEMPLDAO templdao = new TEMPLDAO();
+    String[] columnNames = {"EMPNO","FIRSTNME","MIDINIT","LASTNAME","WORKDEPT","PHONENO","HIREDATE","JOB","EDLEVEL","SEX","BIRTHDATE","SALARY","BONUS","COMM"};
 
 
 
@@ -35,6 +39,12 @@ public class MainTab {
     private JButton btn_update;
     private JButton btn_query;
     private JPanel JPMain;
+    private JTextField textField9;
+    private JTextField textField10;
+    private JTextField textField11;
+    private JTextField textField12;
+    private JTextField textField13;
+    private JTextField textField14;
 
 
     public MainTab() throws SQLException {
@@ -51,13 +61,25 @@ public class MainTab {
             @Override
             public void actionPerformed(ActionEvent e) {
                 try {
-                    List<ACT> list = actdao.QueryMultiply("SELECT * FROM act WHERE ACTNO >= ?", ACT.class,10);
+                    List<TEMPL> list = templdao.QueryMultiply("SELECT * FROM employee", TEMPL.class);
 
-                    Object[][] data = new Object[list.size()][3];
+                    Object[][] data = new Object[list.size()][14];
                     for (int i = 0; i < data.length ; i++) {
-                        data[i][0] = list.get(i).getACTNO();
-                        data[i][1] = list.get(i).getACTKWD();
-                        data[i][2] = list.get(i).getACTDESC();
+                        data[i][0] = list.get(i).getEMPNO();
+                        data[i][1] = list.get(i).getFIRSTNME();
+                        data[i][2] = list.get(i).getMIDINIT();
+                        data[i][3] = list.get(i).getLASTNAME();
+                        data[i][4] = list.get(i).getWORKDEPT();
+                        data[i][5] = list.get(i).getPHONENO();
+                        data[i][6] = list.get(i).getHIREDATE();
+                        data[i][7] = list.get(i).getJOB();
+                        data[i][8] = list.get(i).getEDLEVEL();
+                        data[i][9] = list.get(i).getSEX();
+                        data[i][10] = list.get(i).getBIRTHDATE();
+                        data[i][11] = list.get(i).getSALARY();
+                        data[i][12] = list.get(i).getBONUS();
+                        data[i][13] = list.get(i).getCOMM();
+
                         tabData.setModel(new DefaultTableModel(data, columnNames));
 
                     }
@@ -88,22 +110,37 @@ public class MainTab {
             public void valueChanged(ListSelectionEvent e) {
                 int selrow = tabData.getSelectedRow();
                 if (selrow > -1) {
-                    setACTvalues(selrow);
+                    setTEMPLvalues(selrow);
                 }
             }
         });
         btn_add.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                String actno = textField1.getText();
-                String actkwd = textField2.getText();
-                String actdesc = textField3.getText();
-                ACT act = new ACT(Integer.valueOf(actno),actkwd,actdesc);
+
+                String EMPNO = textField1.getText();
+                String FIRSTNME = textField2.getText();
+                String MIDINIT = textField3.getText();
+                String LASTNAME = textField4.getText();
+                String WORKDEPT = textField5.getText();
+                String PHONENO = textField6.getText();
+                String HIREDATE = textField7.getText();
+                String JOB = textField8.getText();
+                String EDLEVEL = textField9.getText();
+                String SEX = textField10.getText();
+                String BIRTHDATE = textField11.getText();
+                String SALARY = textField12.getText();
+                String BONUS = textField13.getText();
+                String COMM = textField14.getText();
+
+                TEMPL templ = new TEMPL(EMPNO,FIRSTNME,MIDINIT,LASTNAME,WORKDEPT,PHONENO,  java.sql.Date.valueOf(HIREDATE),JOB,Integer.valueOf(EDLEVEL),SEX,java.sql.Date.valueOf(BIRTHDATE),SALARY,BONUS,COMM);
+
+//                ACT act = new ACT(Integer.valueOf(actno),actkwd,actdesc);
 //                System.out.println(act);
 //                ACTDAO actDAO = new ACTDAO();
 //        System.out.println(act.getACTNO());
                 try {
-                    int rows = actdao.Update("INSERT INTO act (ACTNO,ACTKWD,ACTDESC) VALUES(?,?,?)",act.getACTNO(),act.getACTKWD(),act.getACTDESC());
+                    int rows = templdao.Update("INSERT INTO employee (EMPNO,FIRSTNME,MIDINIT,LASTNAME,WORKDEPT,PHONENO, HIREDATE,JOB,EDLEVEL,SEX,BIRTHDATE,SALARY,BONUS,COMM) VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?)",templ.getEMPNO(),templ.getFIRSTNME(),templ.getMIDINIT(), templ.getLASTNAME(),templ.getWORKDEPT(),templ.getPHONENO(),templ.getHIREDATE(),templ.getJOB(),templ.getEDLEVEL(),templ.getSEX(),templ.getBIRTHDATE(),templ.getSALARY(),templ.getBONUS(),templ.getCOMM());
                     if(rows>0){
                         JOptionPane.showMessageDialog(null,"Greetings");
                     }
@@ -121,15 +158,26 @@ public class MainTab {
         btn_update.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                String actno = textField1.getText();
-                String actkwd = textField2.getText();
-                String actdesc = textField3.getText();
-                ACT act = new ACT(Integer.valueOf(actno),actkwd,actdesc);
+                String EMPNO = textField1.getText();
+                String FIRSTNME = textField2.getText();
+                String MIDINIT = textField3.getText();
+                String LASTNAME = textField4.getText();
+                String WORKDEPT = textField5.getText();
+                String PHONENO = textField6.getText();
+                String HIREDATE = textField7.getText();
+                String JOB = textField8.getText();
+                String EDLEVEL = textField9.getText();
+                String SEX = textField10.getText();
+                String BIRTHDATE = textField11.getText();
+                String SALARY = textField12.getText();
+                String BONUS = textField13.getText();
+                String COMM = textField14.getText();
+                TEMPL templ = new TEMPL(EMPNO,FIRSTNME,MIDINIT,LASTNAME,WORKDEPT,PHONENO,  java.sql.Date.valueOf(HIREDATE),JOB,Integer.valueOf(EDLEVEL),SEX,java.sql.Date.valueOf(BIRTHDATE),SALARY,BONUS,COMM);
 //                System.out.println(act);
 //                ACTDAO actDAO = new ACTDAO();
 //        System.out.println(act.getACTNO());
                 try {
-                    int rows = actdao.Update("update act set actkwd = ? where actno = ?",act.getACTKWD(),act.getACTNO());
+                    int rows = templdao.Update("update employee set sex = ? where empno = ?",templ.getSEX(),templ.getEMPNO());
                     if(rows>0){
                         JOptionPane.showMessageDialog(null,"Greetings");
                     }
@@ -146,15 +194,26 @@ public class MainTab {
             @Override
             public void actionPerformed(ActionEvent e) {
                 //delete from act where actno = 111
-                String actno = textField1.getText();
-                String actkwd = textField2.getText();
-                String actdesc = textField3.getText();
-                ACT act = new ACT(Integer.valueOf(actno),actkwd,actdesc);
+                String EMPNO = textField1.getText();
+                String FIRSTNME = textField2.getText();
+                String MIDINIT = textField3.getText();
+                String LASTNAME = textField4.getText();
+                String WORKDEPT = textField5.getText();
+                String PHONENO = textField6.getText();
+                String HIREDATE = textField7.getText();
+                String JOB = textField8.getText();
+                String EDLEVEL = textField9.getText();
+                String SEX = textField10.getText();
+                String BIRTHDATE = textField11.getText();
+                String SALARY = textField12.getText();
+                String BONUS = textField13.getText();
+                String COMM = textField14.getText();
+                TEMPL templ = new TEMPL(EMPNO,FIRSTNME,MIDINIT,LASTNAME,WORKDEPT,PHONENO,  java.sql.Date.valueOf(HIREDATE),JOB,Integer.valueOf(EDLEVEL),SEX,java.sql.Date.valueOf(BIRTHDATE),SALARY,BONUS,COMM);
 //                System.out.println(act);
 //                ACTDAO actDAO = new ACTDAO();
 //               System.out.println(act.getACTNO());
                 try {
-                    int rows = actdao.Update("delete from act where actno = ?",act.getACTNO());
+                    int rows = templdao.Update("delete from employee where empno = ?",templ.getEMPNO());
                     if(rows>0){
                         JOptionPane.showMessageDialog(null,"Greetings");
                     }
@@ -186,13 +245,24 @@ public class MainTab {
     }
 
     private  void InitTable() throws SQLException {
-        List<ACT> list = actdao.QueryMultiply("SELECT * FROM act WHERE ACTNO >= ?", ACT.class,20);
+        List<TEMPL> list = templdao.QueryMultiply("SELECT * FROM employee", TEMPL.class);
 
-        Object[][] data = new Object[list.size()][3];
+        Object[][] data = new Object[list.size()][14];
         for (int i = 0; i < data.length ; i++) {
-            data[i][0] = list.get(i).getACTNO();
-            data[i][1] = list.get(i).getACTKWD();
-            data[i][2] = list.get(i).getACTDESC();
+            data[i][0] = list.get(i).getEMPNO();
+            data[i][1] = list.get(i).getFIRSTNME();
+            data[i][2] = list.get(i).getMIDINIT();
+            data[i][3] = list.get(i).getLASTNAME();
+            data[i][4] = list.get(i).getWORKDEPT();
+            data[i][5] = list.get(i).getPHONENO();
+            data[i][6] = list.get(i).getHIREDATE();
+            data[i][7] = list.get(i).getJOB();
+            data[i][8] = list.get(i).getEDLEVEL();
+            data[i][9] = list.get(i).getSEX();
+            data[i][10] = list.get(i).getBIRTHDATE();
+            data[i][11] = list.get(i).getSALARY();
+            data[i][12] = list.get(i).getBONUS();
+            data[i][13] = list.get(i).getCOMM();
 
         }
         DefaultTableModel dtm = new DefaultTableModel(data, columnNames);
@@ -200,7 +270,7 @@ public class MainTab {
     }
 
 
-    private void setACTvalues(int selrow) {
+    private void setTEMPLvalues(int selrow) {
         if (tabData.getValueAt(selrow, 0) != null) {
             textField1.setText(tabData.getValueAt(selrow,0).toString());
 
@@ -211,6 +281,50 @@ public class MainTab {
         }
         if (tabData.getValueAt(selrow, 2) != null) {
             textField3.setText(tabData.getValueAt(selrow,2).toString());
+
+        }
+        if (tabData.getValueAt(selrow, 3) != null) {
+            textField4.setText(tabData.getValueAt(selrow,3).toString());
+
+        }
+        if (tabData.getValueAt(selrow, 4) != null) {
+            textField5.setText(tabData.getValueAt(selrow,4).toString());
+
+        }
+        if (tabData.getValueAt(selrow, 5) != null) {
+            textField6.setText(tabData.getValueAt(selrow,5).toString());
+
+        }
+        if (tabData.getValueAt(selrow, 6) != null) {
+            textField7.setText(tabData.getValueAt(selrow,6).toString());
+
+        }
+        if (tabData.getValueAt(selrow, 7) != null) {
+            textField8.setText(tabData.getValueAt(selrow,7).toString());
+
+        }
+        if (tabData.getValueAt(selrow, 8) != null) {
+            textField9.setText(tabData.getValueAt(selrow,8).toString());
+
+        }
+        if (tabData.getValueAt(selrow, 9) != null) {
+            textField10.setText(tabData.getValueAt(selrow,9).toString());
+
+        }
+        if (tabData.getValueAt(selrow, 10) != null) {
+            textField11.setText(tabData.getValueAt(selrow,10).toString());
+
+        }
+        if (tabData.getValueAt(selrow, 11) != null) {
+            textField12.setText(tabData.getValueAt(selrow,11).toString());
+
+        }
+        if (tabData.getValueAt(selrow, 12) != null) {
+            textField13.setText(tabData.getValueAt(selrow,12).toString());
+
+        }
+        if (tabData.getValueAt(selrow, 13) != null) {
+            textField14.setText(tabData.getValueAt(selrow,13).toString());
 
         }
 //        if (tabData.getValueAt(selrow, 0) != null) {
@@ -226,7 +340,7 @@ public class MainTab {
 //        if (tabData.getValueAt(selrow, 2) != null) {
 //            System.out.print(tabData.getValueAt(selrow,2).toString());
 //            System.out.print(" ");
-
+//
 //        }
     }
 }
